92d92992cbbccc90fc76b4735e63f35c45aaf137
MarinSumiga <87280241+MarinSumiga@users.noreply.github.com>
2025-04-11 19:32:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,31 @@
 const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static("public")); 
-
-app.get('/', (req, res) => {
-    res.send('Pozdrav sa Railway servera!');
-});
-
 const fs = require('fs');
 const path = require('path');
-
+const app = express();
+const PORT = process.env.PORT || 3000;
+ 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.use(express.static('public'));
+ 
 app.get('/', (req, res) => {
-    const dataPath = path.join(__dirname, 'images.json');
-    const images = JSON.parse(fs.readFileSync(dataPath));
-    res.render('slike', { images });
+  res.redirect('/slike');
 });
-
+ 
 app.get('/slike', (req, res) => {
-    const folderPath = path.join(__dirname, 'public', 'images');
-    const files = fs.readdirSync(folderPath);
-
-    const images = files
-        .filter(file => file.endsWith('.jpg') || file.endsWith('.svg'))
-        .map((file, index) => ({
-            url: `/images/${file}`,
-            id: `slika${index + 1}`,
-            title: `Slika ${index + 1}`
-        }));
-
-    res.render('slike', { images });
+  const folderPath = path.join(__dirname, 'public', 'images');
+  const files = fs.readdirSync(folderPath);
+ 
+  const images = files
+    .filter(file => file.endsWith('.svg') || file.endsWith('.png'))
+    .map((file, index) => ({
+      url: `/images/${file}`,
+      id: `slika${index + 1}`,
+      title: `Slika ${index + 1}`
+    }));
+ 
+  res.render('slike', { images });
 });
-
-
+ 
 app.listen(PORT, () => {
-    console.log(`Server pokrenut na localhost:${PORT}`);
+  console.log(`Server pokrenut na http://localhost:${PORT}`);
 });
